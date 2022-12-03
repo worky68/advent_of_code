d13db35c390a972c03013c82eb160e0b637a5d59
worky68 <worky68@gitlab.com>
2022-12-02 20:07:55 -0500

    day 1

--- build1_2.zig
+++ build1_2.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("advent_of_code", "problem1_1.zig");
+    const exe = b.addExecutable("aoc_problem1_2", "problem1_2.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
@@ -25,7 +25,7 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("problem1_1.zig");
+    const exe_tests = b.addTest("problem1_2.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
 
